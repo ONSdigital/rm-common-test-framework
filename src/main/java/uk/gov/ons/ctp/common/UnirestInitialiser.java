18e23259ea625cf62d6e56659abac77726fbf2ff
Ben Jefferies <benjefferies@echosoft.uk>
2018-07-20 10:30:19 +0100

    Allow use of existing objectmapper

--- src/main/java/uk/gov/ons/ctp/common/UnirestInitialiser.java
+++ src/main/java/uk/gov/ons/ctp/common/UnirestInitialiser.java
@@ -1,19 +1,29 @@
 package uk.gov.ons.ctp.common;
 
-import com.google.gson.Gson;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mashape.unirest.http.Unirest;
 
+import java.io.IOException;
+
 public class UnirestInitialiser {
-  public static void initialise() {
-    final Gson gson = new Gson();
+  public static void initialise(final ObjectMapper mapper) {
     Unirest.setObjectMapper(
         new com.mashape.unirest.http.ObjectMapper() {
           public <T> T readValue(final String value, final Class<T> valueType) {
-            return gson.fromJson(value, valueType);
+            try {
+              return mapper.readValue(value, valueType);
+            } catch (IOException e) {
+              throw new RuntimeException(e);
+            }
           }
 
           public String writeValue(final Object value) {
-            return gson.toJson(value);
+            try {
+              return mapper.writeValueAsString(value);
+            } catch (JsonProcessingException e) {
+              throw new RuntimeException(e);
+            }
           }
         });
   }
